78c336e2eba3d6168cae2d60328c59ad89b76ebc
Ferenc Grecu <mivor20@gmail.com>
2013-11-28 19:44:42 +0200

    Working WeakAlg for 3 points

--- GeomComp/LibConvexHull.cs
+++ GeomComp/LibConvexHull.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace GeomComp
 {
-    class LibConvexHull
+    public class LibConvexHull
     {
+        public List<Point> Hull { get; private set; }
+
+        public void ExecWeakAlg(List<Point> _pointList)
+        {
+            if (_pointList.Count < 4)
+            {
+                this.Hull = _pointList; 
+            }
+        }
     }
 }
--- TestsGeomComp/BehaviorLibConvexHull.cs
+++ TestsGeomComp/BehaviorLibConvexHull.cs
@@ -5,10 +5,78 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using GeomComp;
+using System.Drawing;
 
 namespace TestsGeomComp
 {
+    [TestFixture]
     public class BehaviorLibConvexHull
     {
+        LibConvexHull chLib;
+        List<Point> stubPointCloudList;
+
+        [SetUp]
+        public void SetUp()
+        {
+            chLib = new LibConvexHull();
+            stubPointCloudList = new List<Point>();
+        }
+
+        private List<Point> _stubThreePoints()
+        {
+            List<List<Point>> data = new List<List<Point>>();
+            List<Point> stub = new List<Point>();
+            stub.Add(new Point(40, 50));
+            stub.Add(new Point(10, 20));
+            stub.Add(new Point(200, 30));
+            data.Add(stub);
+            return stub;
+        }
+
+        [TestCaseSource("_stubThreePoints")]
+        public void WeakAlg_shouldReturnPoints_forOnePoint()
+        {
+            stubPointCloudList.Add(new Point(40,50));
+
+            chLib.ExecWeakAlg(stubPointCloudList);
+
+            Assert.That(chLib.Hull, Is.EqualTo(stubPointCloudList));
+        }
+
+        [Test]
+        public void WeakAlg_shouldReturnPoints_forTwoPoints()
+        {
+            stubPointCloudList.Add(new Point(40, 50));
+            stubPointCloudList.Add(new Point(10, 20));
+
+            chLib.ExecWeakAlg(stubPointCloudList);
+
+            Assert.That(chLib.Hull, Is.EqualTo(stubPointCloudList));
+        }
+
+        [Test]
+        public void WeakAlg_shouldReturnPoints_forThreePoints()
+        {
+            stubPointCloudList.Add(new Point(40, 50));
+            stubPointCloudList.Add(new Point(10, 20));
+            stubPointCloudList.Add(new Point(200, 30));
+
+            chLib.ExecWeakAlg(stubPointCloudList);
+
+            Assert.That(chLib.Hull, Is.EqualTo(stubPointCloudList));
+        }
+
+        [Test]
+        public void WeakAlg_shouldNotReturnPoints_forMinFourPoints()
+        {
+            stubPointCloudList.Add(new Point(10, 10));
+            stubPointCloudList.Add(new Point(40, 10));
+            stubPointCloudList.Add(new Point(10, 40));
+            stubPointCloudList.Add(new Point(11, 11));
+
+            chLib.ExecWeakAlg(stubPointCloudList);
+
+            Assert.That(chLib.Hull, Is.Not.EqualTo(stubPointCloudList));
+        }
     }
 }
