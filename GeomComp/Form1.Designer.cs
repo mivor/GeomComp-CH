d5a6f373b893cb189b7494808cb344f5d8545901
Ferenc Grecu <mivor20@gmail.com>
2013-11-28 18:01:18 +0200

    Stub for Form1 with PictureBox and Bitmap for display

--- GeomComp/Form1.Designer.cs
+++ GeomComp/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 281);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
     }
 }
 
--- GeomComp/Form1.cs
+++ GeomComp/Form1.cs
@@ -12,9 +12,16 @@ namespace GeomComp
 {
     public partial class Form1 : Form
     {
+        private Image image;
+
         public Form1()
         {
             InitializeComponent();
+            image = new Bitmap(pictureBox.Width, pictureBox.Height); // 250, 250
+        }
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(image, 0, 0);
         }
     }
 }
