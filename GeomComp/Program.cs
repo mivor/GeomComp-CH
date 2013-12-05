34a45f575e691e8f35caa40ddfe1c04e4ce80f0b
Ferenc Grecu <mivor20@gmail.com>
2013-12-05 19:37:26 +0200

    Add stub program for debuging

--- GeomComp/Program.cs
+++ GeomComp/Program.cs
@@ -17,6 +17,18 @@ namespace GeomComp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            List<System.Drawing.Point> stubPointCloud = new List<System.Drawing.Point>();
+            WeakConvexHull weakCH = new WeakConvexHull();
+
+            stubPointCloud.Add(new System.Drawing.Point(10, 10));
+            stubPointCloud.Add(new System.Drawing.Point(40, 10));
+            stubPointCloud.Add(new System.Drawing.Point(10, 40));
+            stubPointCloud.Add(new System.Drawing.Point(11, 11));
+            stubPointCloud.Add(new System.Drawing.Point(10, 30));
+            stubPointCloud.Add(new System.Drawing.Point(30, 10));
+
+            weakCH.ExecWeakAlg(stubPointCloud);
         }
     }
 }
