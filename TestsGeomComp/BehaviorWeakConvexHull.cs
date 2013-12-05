8be01b9ea853f29ca9322177605535005f65827b
Ferenc Grecu <mivor20@gmail.com>
2013-12-05 17:24:42 +0200

    Refractor test names

--- TestsGeomComp/BehaviorWeakConvexHull.cs
+++ TestsGeomComp/BehaviorWeakConvexHull.cs
@@ -13,13 +13,13 @@ namespace TestsGeomComp
     public class BehaviorWeakConvexHull
     {
         WeakConvexHull weakCH;
-        List<Point> stubPointCloudList;
+        List<Point> stubPointCloud;
 
         [SetUp]
         public void SetUp()
         {
             weakCH = new WeakConvexHull();
-            stubPointCloudList = new List<Point>();
+            stubPointCloud = new List<Point>();
         }
 
         private List<List<Point>> _stubThreePoints()
@@ -38,7 +38,7 @@ namespace TestsGeomComp
         }
 
         [TestCaseSource("_stubThreePoints")]
-        public void WeakAlg_shouldReturnPoints_upToThreepoints(List<Point> _stubPoints)
+        public void shouldReturnPoints_upToThreepoints(List<Point> _stubPoints)
         {
             weakCH.ExecWeakAlg(_stubPoints);
 
@@ -46,16 +46,16 @@ namespace TestsGeomComp
         }
 
         [Test]
-        public void WeakAlg_shouldNotReturnPoints_forMinFourPoints()
+        public void shouldNotReturnPoints_forMinFourPoints()
         {
-            stubPointCloudList.Add(new Point(10, 10));
-            stubPointCloudList.Add(new Point(40, 10));
-            stubPointCloudList.Add(new Point(10, 40));
-            stubPointCloudList.Add(new Point(11, 11));
+            stubPointCloud.Add(new Point(10, 10));
+            stubPointCloud.Add(new Point(40, 10));
+            stubPointCloud.Add(new Point(10, 40));
+            stubPointCloud.Add(new Point(11, 11));
 
-            weakCH.ExecWeakAlg(stubPointCloudList);
+            weakCH.ExecWeakAlg(stubPointCloud);
 
-            Assert.That(weakCH.Hull, Is.Not.EqualTo(stubPointCloudList));
+            Assert.That(weakCH.Hull, Is.Not.EqualTo(stubPointCloud));
         }
     }
 }
